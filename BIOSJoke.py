81d449624a0d49d28a8f88ba29b12fb0f21ce194
duckida <61168649+duckida@users.noreply.github.com>
2020-02-17 15:28:18 -0800

    Add files via upload
    added orignals

---
+++ BIOSJoke.py
@@ -0,0 +1,8 @@
+from time import sleep
+print("Tier 1 BIOS/Bootloader")
+print("Trying to boot from IDE0/SAMSUNGM23430 - Device not found...")
+sleep(1)
+print("Trying to boot from IDE1/WIN95FULL - Device not found")
+sleep(1)
+print("Booting IDE2/MACOS10_15_3")
+print("")
---
+++ terminal.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+while True:
+    cmd = input("$")
+    if cmd == "help":
+        print("Commands are ls, info, and help")
+    elif cmd == "ls":
+        print("Joke.txt beep.png Documents")
+    elif cmd == "info":
+        print("PyTERM 1.0 on Windows 95")
+
+    else:
+        print("Erasing disk C:")
+        sleep(1)
+        print("Complete")
+        break
